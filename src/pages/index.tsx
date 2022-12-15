a732aa0ed191b4e49f84a9b7f80026fd168bcf42
delimamatheus <matheus.lima@unigran.br>
2022-12-15 15:11:27 -0300

    🚧 add content to `index`

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
-import { Box, Text } from '@chakra-ui/react'
+import { Box, Center, Text } from '@chakra-ui/react'
 import Head from 'next/head'
 import styles from '../../styles/Home.module.css'
+import { NotificationBox } from '../components/pages/Home'
 import { MainLayout } from '../layout/MainLayout'
 
 export default function Home() {
@@ -13,10 +14,53 @@ export default function Home() {
       </Head>
 
       <MainLayout>
-        <Box className={styles.main}>
-          <Text> CONTENT </Text>
-        </Box>
+        <Center>
+          <Box 
+            bgColor={'#E6FFFA'}
+            w={'800px'}
+            h={'550px'}
+            mt={'100px'}
+            mb={'100px'}
+          >
+            <NotificationBox 
+              image={'https://bit.ly/dan-abramov'}
+              name={'Matheus'}
+              message={' followed you '}
+              action={''}
+              time={'23h ago'}
+            >            
+            </NotificationBox>
 
+            <NotificationBox 
+              image={'https://bit.ly/dan-abramov'}
+              name={'Andrius'}
+              message={' reacted to your recent post '}
+              action={'Game Day!'}
+              time={'1d ago'}
+            >            
+            </NotificationBox>
+
+            <NotificationBox 
+              image={'https://bit.ly/dan-abramov'}
+              name={'Lucas'}
+              message={' sent you a private message '}
+              action={''}
+              time={'1 week ago'}
+            >            
+            </NotificationBox>
+
+            <NotificationBox 
+              image={'https://bit.ly/dan-abramov'}
+              name={'Lucas'}
+              message={' liked your photo '}
+              action={''}
+              time={'1 month ago'}
+            >            
+            </NotificationBox>            
+
+            
+          </Box>
+        </Center>
       </MainLayout>
     </div>
   )
